dfb0d601676dd74cdfe5f4a8dc2ab0edc127a074
Muhammad Hamza Malik <hamzamalik123450@gmail.com>
2026-05-19 15:43:13 +0500

    update commit

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose")
 
-async function connectToDatabase(url) {
-  return  mongoose.connect(url);
+async function connectToDatabase() {
+  return  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("Connected to MongoDB")
+  }).catch((error) => {
+    console.error("Error connecting to MongoDB:", error)
+    throw error
+  });
 }
 
 module.exports = {
