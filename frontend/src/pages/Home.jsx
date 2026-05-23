6af9b4ad6e4e1c06dfaa2c8ca15376275cb0ea4d
Muhammad Hamza Malik <hamzamalik123450@gmail.com>
2026-05-23 16:44:04 +0500

    update APi

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import { Eye, ArrowUpRight, Mail } from "lucide-react";
 import { useState, useEffect } from "react";
 
-const API = `${import.meta.env.VITE_API_URL}/api/homepage`;
+const API = `https://expressjspracticingproject-production.up.railway.app/api/homepage`;
+console.log(API);
+
 
 // ── Image with fallback ──────────────────────────────────────────────────────
 function Avatar({ src, name, radius = 18 }) {
